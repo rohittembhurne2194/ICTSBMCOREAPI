1e422e08f763eaa489e6643257e9cc222bfb0aad
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-12-14 14:48:55 +0530

    Comit By Rohit

--- ICTSBMCOREAPI/Controllers/LoginController.cs
+++ ICTSBMCOREAPI/Controllers/LoginController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ICTSBMCOREAPI.Controllers
@@ -27,9 +28,22 @@ namespace ICTSBMCOREAPI.Controllers
 
         [HttpPost("getToken")]
         [AllowAnonymous]
-        public async Task<IActionResult> Login([FromHeader] int AppId)
+        public async Task<IActionResult> Login([FromHeader] string AppId)
         {
-            var result = await objRep.LoginAsync(AppId);
+            //string encodedStr = AppId.ToString();
+            //string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes(AppId_New));
+
+            string inputStr = Encoding.UTF8.GetString(Convert.FromBase64String(AppId));
+
+            string[] inputStr2 = inputStr.Split('@');
+
+            string[] abc = inputStr2[2].Split('.');
+
+            string inputStr3 = inputStr2[1].Trim() + abc[1].Trim();
+          
+
+            int AppId_New = Convert.ToInt32(inputStr3);
+            var result = await objRep.LoginAsync(AppId_New);
             if (string.IsNullOrEmpty(result))
             {
                 return Unauthorized();
