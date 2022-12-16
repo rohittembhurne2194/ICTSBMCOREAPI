168771f3d2a027d5b5e4cf3514c65891c1978cf1
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-12-16 18:10:43 +0530

    Comit By Rohit

--- 2.Business Layer/ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels/Models/TrailsDetails.cs
+++ 2.Business Layer/ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels/Models/TrailsDetails.cs
@@ -9,7 +9,7 @@ namespace ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels.Models
         public string code { get; set; }
         public string status { get; set; }
         public string message { get; set; }
-        public string errorMessages { get; set; }
+       // public string errorMessages { get; set; }
 
         public string timestamp { get; set; }
         public dynamic data { get; set; }
