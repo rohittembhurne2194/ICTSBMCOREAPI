aba501fe61ab39e624c8f86fa168e916664a402e
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-12-16 13:51:07 +0530

    Comit By Rohit

--- 2.Business Layer/ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels/Models/GisResult.cs
+++ 2.Business Layer/ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels/Models/GisResult.cs
@@ -29,7 +29,7 @@ namespace ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels.Models
         [JsonProperty("type")]
         public string type { get; set; }
         [JsonProperty("coordinates")]
-        public coordinate? coordinates { get; set; }
+        public dynamic coordinates { get; set; }
     }
 
     public class coordinate
