502c2e7b1fc00aa97174b3039b4437eb3875d4af
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-02-14 17:05:44 +0530

    Comit By Rohit

---
+++ 2.Business Layer/ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels/Models/MyGisTrail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICTSBMCOREAPI.SwachhBhart.API.Bll.ViewModels.Models
+{
+    public class MyGisTrail
+    {
+        public List<Dictionary<string, object>> Data { get; set; }
+    }
+}
